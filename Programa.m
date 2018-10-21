e4d0b8a8824bed83f91cd5c684e616d212223a5d
Pedro <pedrohgf@globomail.com>
2018-10-21 18:07:46 -0300

    Adicionado ao script principal as linhas de código para plotar os gráficos Box Constraint vs Acurácia e a curva ROC [carece de revisão]

--- Programa.m
+++ Programa.m
@@ -27,3 +27,51 @@ end
 SVMModelMultiquadratic = fitcsvm(TrainingFeatures,TrainingLabels,'KernelFunction','Multiquadratic','Standardize',true);
 LabelsPredicted = predict(SVMModelGaussian,TestFeatures);
 MultiquadraticRightLabels = sum(strcmp(LabelsPredicted, TestLabels) == 1);
+
+figure(1),
+semilogx(C, 100*(LinearRightLabels./length(TestLabels)));
+title('Box Constraint vs Acurácia (%) - Kernel Linear'),
+xlabel('Box Constraint'),
+ylabel('Acurácia (%)'),
+axis([2^-15 2^5 60 100]),
+
+
+figure(2),
+semilogx(C, 100*(GaussianRightLabels./length(TestLabels)));
+title('Box Constraint vs Acurácia (%) - Kernel Gaussiano'),
+xlabel('Box Constraint'),
+ylabel('Acurácia (%)'),
+axis([2^-15 2^5 60 100]),
+
+%% Gerando Modelos Comparáveis de Kernels Linear, Gaussiano e Multiquadrático
+BooleanTrainingLabels = zeros(size(TrainingLabels));
+BooleanTrainingLabels = strcmp(TrainingLabels, 'B');
+% BooleanTrainingLabels(BooleanTrainingLabels == 'M') = '1'; 
+SVMModelMultiquadratic = fitcsvm(TrainingFeatures,BooleanTrainingLabels,'KernelFunction','Multiquadratic','Standardize',true);
+SVMModelLinear = fitcsvm(TrainingFeatures,BooleanTrainingLabels,'KernelFunction','linear','Standardize',true);
+SVMModelGaussian = fitcsvm(TrainingFeatures,BooleanTrainingLabels,'KernelFunction','gaussian','Standardize',true);
+
+SVMModelMultiquadratic = fitPosterior(SVMModelMultiquadratic);
+[~,MultiquadraticScore] = resubPredict(SVMModelMultiquadratic);
+[Xmulti,Ymulti,Tmulti,AUCmulti] = perfcurve(BooleanTrainingLabels,MultiquadraticScore(:,SVMModelMultiquadratic.ClassNames),'true');
+
+SVMModelLinear = fitPosterior(SVMModelLinear);
+[~,LinearScore] = resubPredict(SVMModelLinear);
+[Xlin,Ylin,Tlin,AUClin] = perfcurve(BooleanTrainingLabels,LinearScore(:,SVMModelLinear.ClassNames),'true');
+
+SVMModelGaussian = fitPosterior(SVMModelGaussian);
+[~,GaussScore] = resubPredict(SVMModelGaussian);
+[Xgauss,Ygauss,Tgauss,AUCgauss] = perfcurve(BooleanTrainingLabels,GaussScore(:,SVMModelGaussian.ClassNames),'true');
+
+figure(3),
+hold on
+plot(Xlin,Ylin),
+plot(Xgauss,Ygauss),
+plot(Xmulti,Ymulti),
+title('Curvas ROC dos Classificadores'),
+xlabel('False Positive Rate'),
+ylabel('True Positive Rate'),
+legend('Kernel Linear', 'Kernel Gaussiano', 'Kernel Multiquadrático'),
+hold off
+
+
